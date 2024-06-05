47cd607071dc169c1953f52fe1544fd9597f3ac6
Jayakanth <jayakanth1812@gmail.com>
2024-06-05 18:25:01 +0530

    day 1

---
+++ day 1/level1.py
@@ -0,0 +1,22 @@
+#1 chceck py version
+#2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3%4)
+print(3**4)
+print(3//4)
+
+#3
+name= "Jayakanth R"
+country= "India"
+
+#4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(name))
+print(type(country))
+print(type(["apple", "banana"]))
