79b7e780f97e99045d59e4e40fe55cf8f0ad3866
Jayakanth <jayakanth1812@gmail.com>
2024-06-08 12:50:46 +0530

    day 2 variables

---
+++ day 2/level 2.py
@@ -0,0 +1,30 @@
+#4
+num_one= 5
+num_two= 4
+
+total= num_one+num_two
+diff= num_one-num_two
+product= num_one*num_two
+division= num_one/num_two
+remainder= num_one%num_two
+exp= num_one**num_two
+floor_division= num_one//num_two
+
+#5
+radius_of_circle= 30
+# formula A=πr2
+area_of_circle= 3.142*radius_of_circle**2
+# formula C=2πr
+circumference_of_circle= 2*3.142*radius_of_circle
+
+print("Area of circle:",area_of_circle)
+print("Circumference of circle:",circumference_of_circle)
+
+# 6
+first_name= input("Enter your first name: ")
+last_name= input("Enter your last name: ")
+age= input("Enter your age: ")
+print("My name is",first_name,last_name,"and I am",age,"years old.")
+
+# 7
+print(help("keywords"))
---
+++ day 2/variables.py
@@ -0,0 +1,24 @@
+#"Day 2:30 Days of python programming"
+firstName= "Jayakanth"
+lastName= "R"
+fullName= "Jayakanth R"
+myCountry= "INDIA"
+city= "Thiruvallur"
+age= 23
+year= 2001
+is_married= False
+is_true= True
+is_light= True
+processor, gpu, ram, ssd,motherboard="ryzen 5900x", "gt710", "d60g 16gb", "980 500gb", "b550 pro"
+
+# print(processor)
+# print(ssd)
+
+
+# print(type(firstName))
+# print(type(age))
+# print(type(is_married))
+
+print(len(firstName))
+print(len(firstName)>len(lastName))
+
