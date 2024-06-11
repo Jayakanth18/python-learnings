b14277cd40dff5a40463df151b7ecef3d2d011e0
Jayakanth <jayakanth1812@gmail.com>
2024-06-11 22:16:46 +0530

    day 4

---
+++ day 4/string.py
@@ -0,0 +1,61 @@
+#1
+print("Thirty"+ " Days" +" Of" " Python")
+#2
+print("Coding" + " For" + " All")
+#3
+company = "Coding For All"
+#4
+print(company)
+#5
+print(len(company))
+#6
+print(company.upper())
+#7
+print(company.lower())
+#8
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+#9
+print(company[0:6])
+#10
+print(company.find("Coding"))
+#11
+print(company.replace("Coding","Python"))
+#12
+print("Python for Everyone".replace("Everyone","All"))
+#13
+print(company.split(" "))
+#14
+print("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(","))
+#15
+print(company[0])
+#16
+print(company[-1])
+#17
+print(company[10])
+#20
+print(company.index("C"))
+#21
+print(company.index("F"))
+#22
+print("Coding For All People".rfind("l"))
+#23
+print('You cannot end a sentence with because because because is a conjunction'.find("because"))
+#24
+print('You cannot end a sentence with because because because is a conjunction'.rfind("because"))
+#25
+print('You cannot end a sentence with because because because is a conjunction'[31:54])
+#26
+print('You cannot end a sentence with because because because is a conjunction'.find("because"))
+#28
+print("Coding For All".startswith("Coding"))
+#29
+print("Coding For All".endswith("Coding"))
+#30
+print('   Coding For All      '.strip())
+#31
+print("30DaysOfPython".isidentifier())
+print("thirty_days_of_python".isidentifier())
+
+
