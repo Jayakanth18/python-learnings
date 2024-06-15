a3edbf10500f7fe6a3d534867b770751732b8138
Jayakanth <jayakanth1812@gmail.com>
2024-06-15 21:51:48 +0530

    updated

---
+++ day 7-Set/set.py
@@ -0,0 +1,8 @@
+#creating set
+empty_set= set()
+empty_set2={"",} # if we gave simple curly brace it taken as dictionary
+print(type(empty_set))
+print(type(empty_set2))
+
+# example
+fruits= {"banana","pine apple","papaya","mango"}
